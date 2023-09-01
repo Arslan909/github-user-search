2d914173894816261a15ef629efbfa32cbf23ea1
Arslan909 <arslanswl909@gmail.com>
2023-09-02 00:03:29 +0500

    fixed the css for responsive design ans some edges case for added and removing profile for favourite users list

--- src/components/LikeList.jsx
+++ src/components/LikeList.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
-export default function LikeList({ likedProfiles }){
-    return(
-        <div className="liked-user-list-container">
-            <h3>Liked Users</h3>
-            <ul>
-                {likedProfiles.map((item) => {
-                    return(
-                        // <li>user: {item.name}</li>
-                        <li>
-                        <img src={item.pfp}/>
-                        <span>{item.name}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+export default function LikeList({ likedProfiles, show, onRemove }) {
+  return (
+    <div className={`liked-user-list-container ${show ? "liked-user-list-visible" : ""}`}>
+      <h3 className="Liked-Users-h3">Liked Users</h3>
+      <ul>
+        {likedProfiles.map((item) => (
+          // <li key={item.id}>user: {item.name}</li>
+          <li key={item.id} className="info-container-in-likedList">
+            <div className="pfp-container">
+              <img alt="user-pfp" className="pfp-image" src={item.pfp} />
+            </div>
+            <h2 className="user-name" ><a className="user-profile-link" href={item.htmlLink} target="_blank">{item.uName}</a></h2>
+            <h5 className="user-name">{item.name}</h5>
+            <span className="follower">follower: {item.follower}</span>{" "}
+            <span className="following">following: {item.following}</span>
+            <p className="repository">repository: {item.repository}</p>
+            <button className="remove-from-likedLisr-btn" onClick={() => onRemove(item.uName)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
